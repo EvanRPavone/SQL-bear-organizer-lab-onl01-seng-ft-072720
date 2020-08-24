972b7c74ac9d90654ae8b4096231b876926ed5cd
Evan Pavone <evanrpavone@gmail.com>
2020-08-24 15:53:39 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE bears (
+  id INTEGER,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+-- CREATE TABLE cats (
+--   name TEXT,
+--   age INTEGER,
+--   breed TEXT, 
+--   net_worth INTEGER
+-- );
